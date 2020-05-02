a79a119c7d3b9b3ac3c1c3054e2b131926a28234
romucampu@gmail <romucampu@gmail.com>
2020-05-02 22:56:14 +0200

    models/
    added routes and authentication for login

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,11 +1,77 @@
 const express = require('express')
 const router = express.Router()
+const auth = require('../../middleware/auth')
+const User = require('../../models/User')
+const jwt = require('jsonwebtoken')
+const config = require('config')
+const bcrypt = require('bcryptjs')
+const { check, validationResult } = require('express-validator')
 
 // @route     GET api/auth
 // @desc      Test route
 // @access    Public
-router.get('/', (request, response) => {
- response.send('Auth route')
+router.get('/', auth, async (request, response) => {
+
+ try {
+  const user = await User.findById(request.user.id).select('-password')
+  response.json(user)
+ } catch (error) {
+  console.error(error.message)
+  response.status(500).json({ message: 'Internal server error' })
+ }
+})
+
+// @route     POST api/auth
+// @desc      Authenticate user and get token
+// @access    Public
+router.post('/', [
+ check('email', 'Please enter a valid email').isEmail(),
+ check('password', 'Password is required').exists()
+], async (request, response) => {
+ const errors = validationResult(request)
+ if (!errors.isEmpty()) {
+  return response.status(400).json({ errors: errors.array() })
+ }
+ const { email, password } = request.body
+
+ try {
+  // See if use exists
+  let user = await User.findOne({ email })
+  if (!user) {
+   return response
+    .status(400)
+    .json({ errors: [{ message: 'Invalid credentials' }] })
+  }
+
+  // Check if there is a match
+  const isMatch = await bcrypt.compare(password, user.password)
+
+  if (!isMatch) {
+   return response
+    .status(400)
+    .json({ errors: [{ message: 'Invalid credentials' }] })
+  }
+
+  // Get payload
+  const payload = {
+   user: {
+    id: user.id
+   }
+  }
+
+  // Return jsonwebtoken
+  jwt.sign(
+   payload,
+   config.get('jwtSecret'),
+   { expiresIn: 360000 },
+   (error, token) => {
+    if (error) throw error
+    response.json({ token })
+   })
+ } catch (error) {
+  response.status(500).send('Internal server error')
+  console.error(error.message)
+ }
 })
 
 module.exports = router
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const router = express.Router()
-const { check, validationResult } = require('express-validator/check')
+const gravatar = require('gravatar')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const config = require('config')
+
+const { check, validationResult } = require('express-validator')
+const User = require('../../models/User')
 
 // @route     POST api/users
 // @desc      Register user
@@ -8,14 +14,60 @@ const { check, validationResult } = require('express-validator/check')
 router.post('/', [
  check('name', 'Name is required').not().isEmpty(),
  check('email', 'Please enter a valid email').isEmail(),
- check('password', 'Enter a password with 6 or more characters').isLength({ min: 6 })
-], (request, response) => {
+ check('password', 'Enter a password with 6 or more characters')
+  .isLength({ min: 6 })
+], async (request, response) => {
  const errors = validationResult(request)
  if (!errors.isEmpty()) {
   return response.status(400).json({ errors: errors.array() })
  }
+ const { name, email, password } = request.body
 
- { response.send('Users route') }
+ try {
+  // See if use exists
+  let user = await User.findOne({ email })
+  if (user) {
+   return response
+    .status(400)
+    .json({ errors: [{ message: 'User already exists' }] })
+  }
+
+  // Get user gravatar
+  const avatar = gravatar.url(email, {
+   s: '200',
+   r: 'pg',
+   d: 'mm'
+  })
+  // Create user
+  user = new User({ name, email, avatar, password })
+
+  // Encrypt password
+  const salt = await bcrypt.genSalt(10)
+  user.password = await bcrypt.hash(password, salt)
+
+  // Save user
+  await user.save();
+
+  // Get payload
+  const payload = {
+   user: {
+    id: user.id
+   }
+  }
+
+  // Return jsonwebtoken
+  jwt.sign(
+   payload,
+   config.get('jwtSecret'),
+   { expiresIn: 360000 },
+   (error, token) => {
+    if (error) throw error
+    response.json({ token })
+   })
+ } catch (error) {
+  response.status(500).send('Internal server error')
+  console.error(error.message)
+ }
 })
 
 module.exports = router
